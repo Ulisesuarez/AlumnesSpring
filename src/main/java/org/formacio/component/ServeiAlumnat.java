324e747693388c1c410dd758ca20dcf7b707fc43
Ulises Suarez <ulises203@msn.com>
2018-05-01 20:05:46 +0200

    Wired and initialized

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,18 +1,25 @@
 package org.formacio.component;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiAlumnat {
-
 	
-	/**
-	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
-	 * el corresponent codi.
-	 * Si el nom de l'alumne es null, no l'ha de donar d'alta
-	 * Retorna true si l'alumne s'ha inserit, false si no.
-	 */
+	@Autowired
+	private RepositoriAlumnesMemoria repoAlumnes;
+	@PostConstruct
+	public void init() {
+		matricula(1, "Antonia");
+		matricula(2, "Joan");
+	}
 	public boolean matricula (int id, String alumne) {
+		if(alumne == null) {
+			return false;
+		}
+		repoAlumnes.altaAlumne(id, alumne);
 		return true;
 	}
 	
